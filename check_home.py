690234ba897b079021c243b3ce9bb2e4891ea5e9
vikasprajapat2 <vikasprajapat34118@gmail.com>
2026-04-27 14:46:23 +0530

    major changes in employe sections and used with emp id

--- check_home.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-try:
-    req = urllib.request.urlopen('http://127.0.0.1:5000/')
-    print('Status:', req.getcode())
-    print(req.read().decode()[:200])
-except Exception as e:
-    print('Error:', e)
--- check_login.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-try:
-    req = urllib.request.urlopen('http://127.0.0.1:5000/login')
-    print('Status:', req.getcode())
-except Exception as e:
-    print('Error:', e)
